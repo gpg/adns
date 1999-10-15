e4b834c406f2aa95f7417fbd7ade69e7690e6ab1
ian <ian>
1999-10-15 16:46:57 +0000

    Include copyright and stuff; change aargh to more normal one.

--- client/fanftest.c
+++ client/fanftest.c
@@ -1,3 +1,36 @@
+/*
+ * fanftest.c
+ * - a small test program from Tony Finch
+ */
+/*
+ *  This file is
+ *   Copyright (C) 1999 Tony Finch <dot@dotat.at>
+ *   Copyright (C) 1999 Ian Jackson <ian@davenant.greenend.org.uk>
+ *
+ *  It is part of adns, which is
+ *    Copyright (C) 1997-1999 Ian Jackson <ian@davenant.greenend.org.uk>
+ *    Copyright (C) 1999 Tony Finch <dot@dotat.at>
+ *  
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software Foundation,
+ *  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This version was originally supplied by Tony Finch, but has been
+ * modified by Ian Jackson as it was incorporated into adns.
+ */
+
+static const char * const cvsid =
+	"$Id$";
 
 #include <sys/types.h>
 #include <sys/time.h>
@@ -11,12 +44,9 @@
 
 static const char *progname;
 
-#define msg(fmt, args...) fprintf(stderr, "%s: " fmt "\n", progname, ##args)
-
-static void aargh(const char *cause) {
-  const char *why = strerror(errno);
-  if (!why) why = "Unknown error";
-  msg("%s: %s (%d)", cause, why, errno);
+static void aargh(const char *msg) {
+  fprintf(stderr, "%s: %s: %s (%d)\n", progname, msg,
+	  strerror(errno) ? strerror(errno) : "Unknown error", errno);
   exit(1);
 }
 
