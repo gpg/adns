e1d3be7eb6f703e8cf9b8104497f010c0298637a
Mark Wooding <mdw@distorted.org.uk>
2014-05-31 16:37:48 +0100

    src/internal.h: Hoist the qcontext definition to before typeinfo.
    So that typeinfo functions can have argument types which involve the
    qcontext structure.
    
    Signed-off-by: Mark Wooding <mdw@distorted.org.uk>

--- src/internal.h
+++ src/internal.h
@@ -124,6 +124,20 @@ typedef struct {
   struct timeval now;
 } parseinfo;
 
+typedef struct {
+  void *ext;
+  void (*callback)(adns_query parent, adns_query child);
+
+  union {
+    adns_rr_addr ptr_addr;
+  } tinfo; /* type-specific state for the query itself: zero-init if you
+	    * don't know better. */
+
+  union {
+    adns_rr_hostaddr *hostaddr;
+  } pinfo; /* state for use by parent's callback function */
+} qcontext;
+
 typedef struct typeinfo {
   adns_rrtype typekey;
   const char *rrtname;
@@ -198,20 +212,6 @@ union maxalign {
   void (*fp)(void);
   union maxalign *up;
 } data;
-
-typedef struct {
-  void *ext;
-  void (*callback)(adns_query parent, adns_query child);
-
-  union {
-    adns_rr_addr ptr_addr;
-  } tinfo; /* type-specific state for the query itself: zero-init if you
-	    * don't know better. */
-
-  union {
-    adns_rr_hostaddr *hostaddr;
-  } pinfo; /* state for use by parent's callback function */
-} qcontext;
 
 struct adns__query {
   adns_state ads;
